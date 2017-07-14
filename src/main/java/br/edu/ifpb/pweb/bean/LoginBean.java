39ed67d6aa36bfe3af9e0e45110864e4fad3719f
Pedro Paiva <pedrovsp109@gmail.com>
2017-07-14 12:19:40 -0300

    Implementando a prmeira view do JSF

---
+++ src/main/java/br/edu/ifpb/pweb/bean/LoginBean.java
@@ -0,0 +1,41 @@
+package br.edu.ifpb.pweb.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name="loginBean")
+@RequestScoped
+public class LoginBean {
+	private String login;
+	private String senha;
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String autentique() {
+		if (login.equals("admin") && senha.equals("123")) {
+			return "suucesso?faces-redirect=true";
+		}
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		facesContext.addMessage(
+				null, 
+				new FacesMessage(
+						FacesMessage.SEVERITY_WARN, "Usuário e/ou senha inválidos.", 
+						"Verifique se CAPSLOCK está ativada."
+						)
+				);
+		
+		return null; //volta para a pagina onde está
+	}
+}
